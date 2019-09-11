2d2b3aa3f250fd496b480e01cac0e53edac53693
Lakshamana <guitrompa1@gmail.com>
2019-09-11 14:33:09 -0300

    add response -- test needed

--- UDPClient.java
+++ UDPClient.java
@@ -4,14 +4,26 @@ import java.net.InetAddress;
 
 public class UDPClient {
 	public static void main(String[] args) throws Exception {
-		DatagramSocket ds = new DatagramSocket();
-    String message = "Cadê a meta, Lucas?";
-    String ipAddress = "10.16.4.47";
+    int port = 3000;
+    DatagramSocket ds = new DatagramSocket();
+    String message = "Cara, com 300 reais...";
+    String ipAddress = "10.16.4.255";
     InetAddress ip = InetAddress.getByName(ipAddress);
 
-    int port = 3000;
     DatagramPacket dp = new DatagramPacket(message.getBytes(), message.length(), ip, port);
     ds.send(dp);
+
+    int len = 1024;
+    byte[] buf = new byte[len];
+
+    Syste.out.println("Server listening on port " + port);
+  
+    DatagramSocket ds = new DatagramSocket(port);
+    DatagramPacket dp = new DatagramPacket(buf, len);
+    ds.receive(dp);
+    
+    String str = new String(dp.getData(), 0, dp.getLength());
+    System.out.println(str);
     ds.close();
 	}
 }
--- UDPServer.java
+++ UDPServer.java
@@ -4,15 +4,24 @@ import java.net.InetAddress;
 
 public class UDPServer {
 	public static void main(String[] args) throws Exception {
-		DatagramSocket ds = new DatagramSocket(3000);
+    int port = 3000;
+		DatagramSocket ds = new DatagramSocket(port);
     int len = 1024;
     byte[] buf = new byte[len];
+
+    Syste.out.println("Server listening on port " + port)
     
     DatagramPacket dp = new DatagramPacket(buf, len);
     ds.receive(dp);
     
     String str = new String(dp.getData(), 0, dp.getLength());
     System.out.println(str);
+
+    String response = "NOT FOUND";
+    InetAddress responseAddress = dp.getAddress();
+    int responsePort = dp.getPort();
+    DatagramPacket responseDp = new DatagramPacket(response.getBytes(), response.length(), responseAddress, responsePort);
+    ds.send(dp);
     ds.close();
 	}
 }
